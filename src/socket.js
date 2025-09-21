2eea47e80fcda14fe3a6c8e3b4036b120a997b50
SB <123059890+sbrb@users.noreply.github.com>
2025-09-21 17:25:16 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://your-backend.onrender.com", {
+export const socket = io("https://voice-calling-server.vercel.app", {
   transports: ["websocket"], 
 });
